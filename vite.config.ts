4b29c91080e6d72d7edc84c5c719e0afcfeb941f
Riley Post <riley_post@hotmail.com>
2024-09-03 21:19:33 +1000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,8 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vueNFSA/",
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  // base: "/vueNFSA/",
+  plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
