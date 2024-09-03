f1c76d6baba8509a2b507fb64ecb064bf2d47bfc
Riley Post <riley_post@hotmail.com>
2024-09-03 21:28:01 +1000

    update

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: "/vueNFSA/",
+  base: '/vueNFSA/',
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
