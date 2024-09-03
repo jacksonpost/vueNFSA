b3c1e3824e05a82869c88120059a443590d0e478
Riley Post <riley_post@hotmail.com>
2024-09-03 20:56:58 +1000

    update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,55 +1,61 @@
 <script lang="ts">
 export default {
-	name: 'HelloWorld',
-	props: {
-		msg: String
-	},
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
   data() {
     return {
       theData: {},
-      imgURL: 'https://media.nfsacollection.net/'
+      imgURL: 'https://media.nfsacollection.net/',
+      query: 'https://api.collection.nfsa.gov.au/search?query=',
+      searchString: 'dog',
+      resultSet: []
     }
   },
-	methods: {
-		fetchData() {
+  methods: {
+    fetchData() {
       // use a piece of dynamic data to modify the API call
-      let query = 'https://api.collection.nfsa.gov.au/search?query=dog';
-      console.log(query);
-			fetch(query)
-				.then(response => {
-					// response.json().then(res => console.log(res));
-          response.json().then(res => this.$data.theData = res);
-				})
-				.catch(err => {
-					console.error(err);
-				});
-		},
-	}
-};
-
+      let queryString = this.query + this.searchString
+      console.log('API call: ' + queryString)
+      fetch(queryString)
+        .then((response) => {
+          // response.json().then(res => console.log(res));
+          response.json().then((res) => {
+            this.$data.theData = res
+            this.$data.resultSet = res.results
+            // console.log(res.results)
+          })
+        })
+        .catch((err) => {
+          console.error(err)
+        })
+    }
+  }
+}
 </script>
 
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    
-    <button @click="fetchData">Click Me!</button>
-    
+
+    <input v-model="searchString" placeholder="query" />
+    <button @click="fetchData">fetch data</button>
+
+    <!-- <p>{{ resultSet[0] }}</p> -->
+
     <ul role="list" class="list-v">
       <!-- create a variable called result, 
       loop through the API results and add a list item for each result.
       Use result to access properties like 'title' and 'name' -->
-      <li v-for="result in theData.results" :key="result.title">
-        <p>{{ result.title }}</p>
-        <p>{{ result.name }}</p>
-        <img v-if="result.preview.length>0" v-bind:src='imgURL + result.preview[0].filePath'>
+      <li v-for="(result, index) in resultSet" :key="result[index]">
+        <p>{{ result['title'] }}</p>
+        <p>{{ result['name'] }}</p>
+        <img v-if="result['preview'][0]" v-bind:src="imgURL + result['preview'][0]['filePath']" />
       </li>
     </ul>
-
   </div>
 </template>
-
-
 
 <style scoped>
 h1 {
